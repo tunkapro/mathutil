ee089a2712f112154fbaf771fe29a1dd537b5167
Anh Tuan <truongtuan20042004@gmail.com>
2025-06-12 17:08:34 +0700

    update main code to use recursion and to see green 4ever

--- src/main/java/com/duclh/tool/mathutil/core/MathUtility.java
+++ src/main/java/com/duclh/tool/mathutil/core/MathUtility.java
@@ -10,22 +10,37 @@ public class MathUtility {
     //KO CÓ GIAI THỪA SỐ ÂM
     //0! = 1
     //20! VỪA ĐỦ KIỂU LONG, TỨC LÀ 21! VƯỢT KIỂU LONG
+//    public static long getFactorial(int n) {
+//        //đem code chính ra sửa gọi là REFACTOR/REFACTORING
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("n must be between 0 and 20");
+//         //bạn đưa invalid data, tớ ném ra ngoại lệ, hàm ko đc chạy thành công, ko value trả về
+//
+//        if(n == 0)
+//            return 1;
+//
+//        //sống sót tới đây n = 1..19
+//        //for mà tính
+//        long result = 1; //thuật toán dồn con heo đất
+//        for(int i = 1; i <= n; i++)
+//            result *= i;
+//
+//        return result;
+//    }
+    // đem lên server mà ra màu xanh
+    // 1 code tối ưu ngon vì pass test case
+    //2. test lại thứ từng test rất mất công, nay có script test, khỏe khỏi test lại chỉ biết màu xanh là đc
+    // Đệ quy - Recursion - gọi lại chính mình voi quy mô nhỏ hơn
     public static long getFactorial(int n) {
         //đem code chính ra sửa gọi là REFACTOR/REFACTORING
         if(n < 0 || n > 20)
             throw new IllegalArgumentException("n must be between 0 and 20");
-         //bạn đưa invalid data, tớ ném ra ngoại lệ, hàm ko đc chạy thành công, ko value trả về
+        //bạn đưa invalid data, tớ ném ra ngoại lệ, hàm ko đc chạy thành công, ko value trả về
 
         if(n == 0)
             return 1;
 
-        //sống sót tới đây n = 1..19
-        //for mà tính
-        long result = 1; //thuật toán dồn con heo đất
-        for(int i = 1; i <= n; i++)
-            result *= i;
-
-        return result;
+        return n*getFactorial(n-1);
     }
 
 }
